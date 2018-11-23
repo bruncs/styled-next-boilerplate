da5d9dc084b333425890aad304fa7ba661b77595
Bruno Correia <brunocs@msn.com>
2018-11-23 18:56:32 -0200

    Add more resets to global style

--- styles/global.js
+++ styles/global.js
@@ -4,12 +4,18 @@ const GlobalStyle = createGlobalStyle`
   *{
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
     outline: 0;
+    box-sizing: border-box;
+  }
+  html, body, #__next {
+    width: 100%;
+    height: 100%;
   }
   body {
     background-color: #333;
     font-family: "Montserrat", sans-serif;
+    text-rendering: optimizeLegibility !important;
+    -webkit-font-smoothing: antialiased !important;
   }
 `;
 
